a74d1337559e61660a79852a4f39d0381303a5d8
Julian Maurice <julian.maurice@biblibre.com>
2026-03-11 14:32:55 +0100

    Assign a random ID to each HTML textarea (needed by CKEditor)

--- asset/js/formularium-form-component-type-html.js
+++ asset/js/formularium-form-component-type-html.js
@@ -1,8 +1,17 @@
 (function () {
     'use strict';
 
+    // CKEditor needs a unique id for each textarea
+    function getRandomId () {
+        const u8array = new Uint8Array(4);
+        const randomString = crypto.getRandomValues(u8array).toHex();
+
+        return `formularium-html-${randomString}`;
+    }
+
     document.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll('.formularium-form-component-type-html textarea').forEach(function (el) {
+            el.setAttribute('id', getRandomId());
             CKEDITOR.inline(el);
         });
 
@@ -10,6 +19,7 @@
             el.addEventListener('formularium:component-added', function (ev) {
                 if (ev.target.classList.contains('formularium-form-component-type-html')) {
                     const textarea = ev.target.querySelector('textarea');
+                    textarea.setAttribute('id', getRandomId());
                     CKEDITOR.inline(textarea);
                 }
             });
